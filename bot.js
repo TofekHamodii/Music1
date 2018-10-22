4c7389e4e03aea8ac68a4377cf3b347936e9ab87
TofekHamodii <43014320+TofekHamodii@users.noreply.github.com>
2018-10-22 15:48:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,4 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '3'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(``,"")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
 
 const Util = require('discord.js');
 
@@ -47,7 +18,9 @@ const fs = require('fs');
 
 const gif = require("gif-search");
 
+const client = new Discord.Client({disableEveryone: true});
 
+const prefix = "1";
 /////////////////////////
 ////////////////////////
 
@@ -343,6 +316,7 @@ client.on('message', message => {
     if (message.content === 'help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
+        .setDescription('**برفكس البوت (!)**')
         .addField('play', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
         .addField('disconnect', 'الخروج من رومك الصوتي')
@@ -367,19 +341,5 @@ client.on('message', message => {
       message.channel.send(helpEmbed);
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 client.login(process.env.BOT_TOKEN);
